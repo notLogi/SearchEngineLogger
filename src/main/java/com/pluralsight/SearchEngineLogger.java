fccf2ffa819a2a2b1559a83d51420581ec6446e3
Roger <92322066+notLogi@users.noreply.github.com>
2025-10-07 15:23:49 -0400

    Finished exercise 3.

---
+++ src/main/java/com/pluralsight/SearchEngineLogger.java
@@ -0,0 +1,36 @@
+package com.pluralsight;
+
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+import java.time.*;
+import java.io.*;
+
+public class SearchEngineLogger {
+    public static void main(String[] args) {
+        String logsFile = "logs.txt";
+        LocalDateTime today = LocalDateTime.now();
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy-dd-MM HH:mm:ss");
+        String formattedDate = today.format(fmt);
+        Scanner myScanner = new Scanner(System.in);
+        try{
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(logsFile));
+            bufferedWriter.write(formattedDate + " launch\n");
+            boolean isDone = false;
+            while(!isDone){
+                System.out.println("Enter a search term (X to exit)");
+                String input = myScanner.nextLine();
+                if(input.equalsIgnoreCase("X")) {
+                    isDone = true;
+                    bufferedWriter.write(formattedDate + " exit\n");
+                }
+                bufferedWriter.write(formattedDate + " search: " + input + "\n");
+            }
+            bufferedWriter.close();
+        }
+        catch(IOException ex){
+            System.err.println("Error has occurred.");
+        }
+        myScanner.close();
+
+    }
+}
